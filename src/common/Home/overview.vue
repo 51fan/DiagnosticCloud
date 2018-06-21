9ff424013fca038dab57c14512c8ecf01b525edc
51fan <abc2978723@163.com>
2018-06-21 23:38:47 +0800

    调整首页

--- src/common/Home/overview.vue
+++ src/common/Home/overview.vue
@@ -1,3 +1,17 @@
 <template>
-    <div>欢迎光临</div>
+    <div class="mypanel" style="font-size: xx-large;">
+        <h6>欢迎光临</h6>
+        <div style="text-align: center;">
+            <img src="../../assets/images/industry.jpg" alt="">
+        </div>
+    </div>
+    
 </template>
+
+<style scoped>
+.mypanel {
+  padding-left: 10%;
+  padding-right: 10%;
+}
+</style>
+
